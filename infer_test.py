e176a20e60b4a1bf473e4742941640e8651d14d3
Paul Lorenz <paul@Pauls-MBP.home>
2017-04-21 00:37:22 -0400

    blah

--- infer_test.py
+++ infer_test.py
@@ -3,9 +3,9 @@ import gensim.models as g
 import codecs
 
 #parameters
-model="toy_data/model.bin"
-test_docs="toy_data/test_docs.txt"
-output_file="toy_data/test_vectors.txt"
+model="/Users/paul/Documents/kaggle/quora/enwiki_dbow/doc2vec.bin"
+test_docs = "/Users/paul/Documents/kaggle/quora/question1.txt"
+output_file = "/Users/paul/Documents/kaggle/quora/question1_vectors.txt"
 
 #inference hyper-parameters
 start_alpha=0.01
@@ -17,7 +17,11 @@ test_docs = [ x.strip().split() for x in codecs.open(test_docs, "r", "utf-8").re
 
 #infer test vectors
 output = open(output_file, "w")
+i = 0
 for d in test_docs:
     output.write( " ".join([str(x) for x in m.infer_vector(d, alpha=start_alpha, steps=infer_epoch)]) + "\n" )
+    if i % 500 == 0:
+        print str(i) + " / " + str(len(test_docs)) + "\t\t" + str(d)[:40] + "\n"
+    i += 1
 output.flush()
 output.close()
